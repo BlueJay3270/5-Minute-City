61ccb9528bcaec721d85f9a02adfba6849e6329b
bluejay <bluej3270@gmail.com>
2020-05-23 17:28:19 -0600

    -

--- game.js
+++ game.js
@@ -1,37 +1,36 @@
 const gameState = {
-
+	score: 0
 };
 
-//preload images, sounds, and sprites
+
+
 function preload() {
 
-};
+}
 
-//Create all game elements here. Code will be run on page load.
+
 function create() {
 
-};
+}
 
-//Code here runs each frame.
 function update() {
 
-};
+}
 
-//game configuration
+
 const config = {
-  type: phaser.AUTO,
-  width: 450,
-  height: 500,
-  backgroundColor: 'ffffff',
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 },
-      enableBody: true,
-    },
-    scene: { preload, create, update }
-  };
+	type: Phaser.AUTO,
+	width: 900,
+	height: 500,
+	backgroundColor: "0fb83c",
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+			enableBody: true,
+		}
+	},
+	scene: { preload, create, update }
 };
 
-//makes the game
 const game = new Phaser.Game(config);
