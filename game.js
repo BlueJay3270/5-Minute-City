f3c7c4271b17a221fbaa1642cba276de215e6242
bluejay <bluej3270@gmail.com>
2020-05-23 19:57:38 -0600

    -

--- game.js
+++ game.js
@@ -5,12 +5,14 @@ const gameState = {
 
 
 function preload() {
-
+this.load.image('resident', 'https://lh3.googleusercontent.com/proxy/iNJjk_B1OH5fDzZF91p0l8LgwOHWZxS2_IL30a6ITowvnJVMoZZ1oeTcbxJOZrSCBKPhI6njI0B-jktUKxRNbjute48CxrORctRvGA4Q_gTx')
 }
 
 
 function create() {
+	const resident = this.physics.add.staticGroup();
 
+	resident.create(200, 200, 'resident').setScale(.5);
 }
 
 function update() {
